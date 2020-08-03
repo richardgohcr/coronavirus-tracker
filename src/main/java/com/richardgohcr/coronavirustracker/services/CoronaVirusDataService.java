089c2e0c40dc997b7f7ecf21ac1c789aa0feab29
richardgohcr <richard.gohcr@gmail.com>
2020-08-03 13:49:55 +0800

    Completed html template and table

--- src/main/java/com/richardgohcr/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/com/richardgohcr/coronavirustracker/services/CoronaVirusDataService.java
@@ -24,6 +24,9 @@ public class CoronaVirusDataService {
 
     private List<LocationStats> allStats = new ArrayList<>();
 
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
 
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
@@ -34,7 +37,6 @@ public class CoronaVirusDataService {
                 .uri(URI.create(VIRUS_DATA_URL))
                 .build();
         HttpResponse<String> httpResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
 
         StringReader csvBodyReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
@@ -42,11 +44,15 @@ public class CoronaVirusDataService {
             LocationStats locationStat = new LocationStats();
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
-            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
-            System.out.println(locationStat);
+            int latestCases = Integer.parseInt(record.get(record.size()-1));
+            int prevDayCases = Integer.parseInt(record.get(record.size()-2));
+
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases-prevDayCases);
             newStats.add(locationStat);
         }
         this.allStats = newStats;
 
     }
+
 }
